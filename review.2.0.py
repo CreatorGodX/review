babb3093bb2fe809f2f0c199f8f5d1b352c86a47
Creator X <godofcreation980316@gmail.com>
2024-08-06 11:08:43 +0800

    Add files via upload

--- review.2.0.py
+++ review.2.0.py
@@ -17,5 +17,9 @@ average = round(number / LEN, 1)
 
 print('the comment lenght average is', average, ':D')
 
-
-
+great = []
+with open ('reviews.txt', 'r') as g:
+    for line in g:
+        if 'great' in line:
+            great.append(line)
+print('there are', len(great), 'comment with "great" here :D')
